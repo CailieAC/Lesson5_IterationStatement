46d81580b928295f6181b89c58f09bba88aad537
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-13 20:30:20 -0600

    commit1

--- Lesson5_IterationStatement/Program.cs
+++ Lesson5_IterationStatement/Program.cs
@@ -6,7 +6,24 @@ namespace Lesson5_IterationStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i==7)
+                {
+                    Console.WriteLine("Found seven!");
+                    //break breaks out of all the loops and iterations
+                    break;
+                }
+            }
+
+            //can type for, then tab tab to auto insert template for iteration
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+
+            Console.ReadLine();
         }
     }
 }
